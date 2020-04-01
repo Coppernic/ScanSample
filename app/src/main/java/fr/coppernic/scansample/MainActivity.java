00a1de554191a1bd6f3d0a63b9fba4f7a26e7d6a
Jean-François Poughon <jean-francois.poughon@coppernic.fr>
2020-04-01 18:48:54 +0200

    Fix #4 Add a way to start and stop barcode service

--- app/src/main/java/fr/coppernic/scansample/MainActivity.java
+++ app/src/main/java/fr/coppernic/scansample/MainActivity.java
@@ -12,7 +12,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +22,11 @@ import fr.coppernic.sdk.utils.core.CpcResult.RESULT;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+    
+    public static final String ACTION_SERVICE_START = "fr.coppernic.intent.action.start.barcode.service";
+    public static final String ACTION_SERVICE_STOP = "fr.coppernic.intent.action.stop.barcode.service";
+    public static final String INTENT_SERVICE_STARTED = "fr.coppernic.intent.barcode.service.STARTED";
+    public static final String INTENT_SERVICE_STOPPED = "fr.coppernic.intent.barcode.service.STOPPED";
     public static final String ACTION_SCAN_SUCCESS = "fr.coppernic.intent.scansuccess";
     public static final String ACTION_SCAN_ERROR = "fr.coppernic.intent.scanfailed";
     public static final String INTENT_ACTION_SCAN = "fr.coppernic.intent.action.SCAN";
@@ -31,6 +38,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String BARCODE_PERMISSION = "fr.coppernic.permission.BARCODE";
     private static final int BARCODE_CODE_PERMISSION = 42;
     public final AndroidInteractor androidInteractor = new AndroidInteractor();
+    private boolean isServiceRunning = true;
+    private String packageNameBarcodeManager = "";
+    private Button btnStartStop = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +61,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        btnStartStop = findViewById(R.id.btnStartStop);
+        btnStartStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startStop();
+            }
+        });
+        // We are checking then for CpcSystemServices application installed on older Coppernic's product. This
+        // application has the same applicationId than CoreService which in installed on Coppernic's products
+        // released from 2019. This is the reason why we are checking for BarcodeManager first. It is for avoiding
+        // false positive.
+        packageNameBarcodeManager = androidInteractor.isAppInstalled(getApplicationContext(),
+                BASE_NAME_BARCODE_MANAGER);
     }
 
     @Override
@@ -62,6 +86,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         unregisterReceiver(scanResult);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(onOffResult);
     }
 
     private BroadcastReceiver scanResult = new BroadcastReceiver() {
@@ -86,22 +117,32 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private BroadcastReceiver onOffResult = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction() != null) {
+                if (intent.getAction().equals(INTENT_SERVICE_STARTED)) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.service_started),
+                            Toast.LENGTH_LONG).show();
+                    Log.d("ScanSample", "Barcode service started");
+                } else if (intent.getAction().equals(INTENT_SERVICE_STOPPED)) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.service_stopped),
+                            Toast.LENGTH_LONG).show();
+                    Log.d("ScanSample", "Barcode service stopped");
+                }
+            }
+        }
+    };
+
+
     /**
      * Triggers a barcode scan
      **/
-
     private void startScan() {
-
         Context context = this.getApplicationContext();
         // We are checking first for BarcodeManager application. This application is installed on Coppernic's products
         // released from 2019. It is replacing barcode service hosted in CpcSystemServices application.
         String packageNameSystemServices = androidInteractor.isAppInstalled(context, BASE_NAME_SYSTEM_SERVICE);
-        // We are checking then for CpcSystemServices application installed on older Coppernic's product. This
-        // application has the same applicationId than CoreService which in installed on Coppernic's products
-        // released from 2019. This is the reason why we are checking for BarcodeManager first. It is for avoiding
-        // false positive.
-        String packageNameBarcodeManager = androidInteractor.isAppInstalled(context, BASE_NAME_BARCODE_MANAGER);
-
         if (!packageNameBarcodeManager.isEmpty()) {
             Intent scanIntent = new Intent();
             // We need to set package to send an explicit intent.
@@ -116,7 +157,6 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Timber.e("Barcode service in Barcode Manager not found");
             }
-
         } else if (!packageNameSystemServices.isEmpty()) {
             Intent scanIntent = new Intent();
             // We need to set package to send an explicit intent.
@@ -136,11 +176,38 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void startStop() {
+        if (!packageNameBarcodeManager.isEmpty()) {
+            Intent startStopIntent = new Intent();
+            startStopIntent.setPackage(packageNameBarcodeManager);
+            //We are telling who we are to barcode service.
+            startStopIntent.putExtra(KEY_PACKAGE, BuildConfig.APPLICATION_ID);
+            if (isServiceRunning) {
+                isServiceRunning = false;
+                btnStartStop.setText(R.string.start_service);
+                startStopIntent.setAction(ACTION_SERVICE_STOP);
+            } else {
+                isServiceRunning = true;
+                btnStartStop.setText(R.string.stop_service);
+                startStopIntent.setAction(ACTION_SERVICE_START);
+            }
+            ComponentName info = getApplicationContext().startService(startStopIntent);
+            if (info == null) {
+                Timber.e("Barcode service in System Service not found");
+            }
+        }
+    }
+
     private void registerReceiver() {
         IntentFilter filter = new IntentFilter();
         filter.addAction(ACTION_SCAN_SUCCESS);
         filter.addAction(ACTION_SCAN_ERROR);
         registerReceiver(scanResult, filter);
+        //
+        IntentFilter filterOnOff = new IntentFilter();
+        filterOnOff.addAction(INTENT_SERVICE_STARTED);
+        filterOnOff.addAction(INTENT_SERVICE_STOPPED);
+        registerReceiver(onOffResult, filterOnOff);
     }
 
     @Override
