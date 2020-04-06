2a0d7ddff3f5fdfecc2ff98e5f3f11c2f157b5cb
Jean-François Poughon <jean-francois.poughon@coppernic.fr>
2020-04-06 18:30:26 +0200

    Fix #8 disable keyevent on start/stop service button

--- app/src/main/java/fr/coppernic/scansample/MainActivity.java
+++ app/src/main/java/fr/coppernic/scansample/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -73,6 +74,12 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     startStopService();
                 }
+            }
+        });
+        btnStartStop.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                return true;//disable all key event on this button
             }
         });
         AndroidInteractor androidInteractor = new AndroidInteractor(getApplicationContext());
