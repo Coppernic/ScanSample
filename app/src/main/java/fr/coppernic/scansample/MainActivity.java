857a5205fcfb32adecf1858c8080e5597e3dffa1
Blacklybarta <36411261+Blacklybarta@users.noreply.github.com>
2020-04-07 10:09:11 +0200

    Fix #8 disable keyevent on start/stop service button (#9)
    * Fix #8 disable keyevent on start/stop service button
    
    * applying CR change

--- app/src/main/java/fr/coppernic/scansample/MainActivity.java
+++ app/src/main/java/fr/coppernic/scansample/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -73,6 +74,13 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     startStopService();
                 }
+            }
+        });
+        //to avoid Keyboard wedge behavior when Carriage Return is enable in BCManager settings
+        btnStartStop.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                return true;//disable all key event on this button
             }
         });
         AndroidInteractor androidInteractor = new AndroidInteractor(getApplicationContext());
